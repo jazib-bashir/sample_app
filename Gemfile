a664a3a49e5f49468d7ee805e53282fce1513ea3
Jazib Bashir <jazibbashir@gmail.com>
2014-07-17 16:13:07 +0500

    Before Authorization Complete

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'rails', '4.1.2'
 gem 'bootstrap-sass', '2.3.2.0'
 gem 'sprockets', '2.11.0'
 gem 'bcrypt-ruby', '3.1.2'
+gem 'minitest'
 # Use sqlite3 as the database for Active Record
 group :production do
   gem 'pg', '0.15.1'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,32 @@
 class UsersController < ApplicationController
-  
+  before_action :signed_in_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
   end
   
   def create 
-  	@user = User.new(user_params) # Not the final implementation
-  	if @user.save
-  		flash[:success] = "Welcome to the Sample App!"
-  	  redirect_to @user
-  	else
-  		render 'new'
-  	end
+  	@user = User.new(user_params)
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+   def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def new
@@ -20,7 +35,13 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    # Before filters
+
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
 end
